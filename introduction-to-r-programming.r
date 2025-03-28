8f18ff985b458ee1cc2b8bee9f3378966664f37d
Rifat Rudro <74897206+RifatRudro@users.noreply.github.com>
2025-03-28 11:20:47 +0600

    Introduction To R Programming | Version 1

---
+++ introduction-to-r-programming.r
@@ -0,0 +1,133 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "d32c0c6f",
+   "metadata": {
+    "execution": {
+     "iopub.execute_input": "2025-03-28T05:20:40.043409Z",
+     "iopub.status.busy": "2025-03-28T05:20:40.041320Z",
+     "iopub.status.idle": "2025-03-28T05:20:40.218048Z",
+     "shell.execute_reply": "2025-03-28T05:20:40.215756Z"
+    },
+    "papermill": {
+     "duration": 0.184281,
+     "end_time": "2025-03-28T05:20:40.221263",
+     "exception": false,
+     "start_time": "2025-03-28T05:20:40.036982",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[1] \"x is positive\"\n"
+     ]
+    }
+   ],
+   "source": [
+    "x <- 5\n",
+    "if (x > 0) {\n",
+    "  print(\"x is positive\")\n",
+    "}\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "9233e6ee",
+   "metadata": {
+    "execution": {
+     "iopub.execute_input": "2025-03-28T05:20:40.257577Z",
+     "iopub.status.busy": "2025-03-28T05:20:40.226058Z",
+     "iopub.status.idle": "2025-03-28T05:20:40.280876Z",
+     "shell.execute_reply": "2025-03-28T05:20:40.278016Z"
+    },
+    "papermill": {
+     "duration": 0.06107,
+     "end_time": "2025-03-28T05:20:40.283927",
+     "exception": false,
+     "start_time": "2025-03-28T05:20:40.222857",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[1] \"X is Positive\"\n"
+     ]
+    }
+   ],
+   "source": [
+    "x <- 4\n",
+    "if(x>0){\n",
+    "    print(\"X is Positive\")\n",
+    "}else{\n",
+    "    print(\"X is Negative\")\n",
+    "}\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "508285b9",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001467,
+     "end_time": "2025-03-28T05:20:40.287005",
+     "exception": false,
+     "start_time": "2025-03-28T05:20:40.285538",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30749,
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.802621,
+   "end_time": "2025-03-28T05:20:40.409115",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2025-03-28T05:20:36.606494",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
